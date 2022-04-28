38db4219cefd915fe222db73b8183586071739e2
Serhii <89796587+SerhiiSakhno1994@users.noreply.github.com>
2022-04-28 21:37:37 +0300

    react2-2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,105 @@
-import './App.css';
+import React from 'react';
+import { nanoid } from 'nanoid';
 
-function App() {
-  return <div className="App">гргргшр</div>;
+import Container from './components/Container';
+import TodoList from './components/TodoList';
+import TodoEditor from './components/TodoEditor/TodoEditor';
+import Filter from './components/Filter';
+// import Form from './components/Form';
+import initalTodos from './components/todos.json';
+
+class App extends React.Component {
+  state = {
+    todos: initalTodos,
+    filter: '',
+  };
+
+  addTodo = text => {
+    console.log(text);
+    const todo = {
+      id: nanoid(),
+      text,
+      completed: false,
+    };
+    this.setState(({ todos }) => ({
+      todos: [todo, ...todos],
+    }));
+  };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+  toggleCompleted = todoId => {
+    console.log(todoId);
+    // this.setState(prevState => ({
+    //   todos: prevState.todos.map(todo => {
+    //     if (todo.id === todoId) {
+    //       return {
+    //         ...todo,
+    //         completed: !todo.completed,
+    //       };
+    //     }
+    //     return todo;
+    //   }),
+    // }));
+
+    this.setState(({ todos }) => ({
+      todos: todos.map(todo =>
+        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    }));
+  };
+
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleTodos = () => {
+    const { todos, filter } = this.state;
+    const normalizeFilter = filter.toLowerCase();
+    return todos.filter(todo =>
+      todo.text.toLowerCase().includes(normalizeFilter),
+    );
+  };
+
+  calculeteCompledetTodoCaunt = () => {
+    const { todos } = this.state;
+    return todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0,
+    );
+  };
+
+  render() {
+    const { todos, filter } = this.state;
+    const totalTodoCount = todos.length;
+    const completedTodoCount = this.calculeteCompledetTodoCaunt();
+    const visibleTodos = this.getVisibleTodos();
+
+    return (
+      <Container>
+        {/* <Form onSubmit={this.formSubmitHandler} /> */}
+        <div>
+          <p>Загальна кількість:{totalTodoCount} </p>
+          <p>Кількість виконаних:{completedTodoCount} </p>
+        </div>
+        <TodoEditor onSubmit={this.addTodo} />
+        <Filter value={filter} onChange={this.changeFilter} />
+        <TodoList
+          todos={visibleTodos}
+          onDeleteTodo={this.deleteTodo}
+          onToggleCompleted={this.toggleCompleted}
+        />
+      </Container>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,26 @@
+import classNames from 'classnames';
+import './TodoList.css';
+
+const TodoList = ({ todos, onDeleteTodo, onToggleCompleted }) => (
+  <ul className="TodoLis">
+    {todos.map(({ id, text, completed }) => (
+      <li
+        key={id}
+        className={classNames('TodoList__item', {
+          'TodoList__item--completed': completed,
+        })}
+      >
+        <input
+          type="checkbox"
+          className="TodoList__checkbox"
+          checked={completed}
+          onChange={() => onToggleCompleted(id)}
+        />
+        <p className="TodoList__text">{text}</p>
+        <button onClick={() => onDeleteTodo(id)}>Удалить</button>
+      </li>
+    ))}
+  </ul>
+);
+
+export default TodoList;
